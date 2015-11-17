98875775b3f32c21f2ba4aaa5236ebcec0791d97
robertmujica <robert.mujica@gmail.com>
2015-11-17 23:10:13 +0000

    Adding Query text to console.log for validation

--- server.js
+++ server.js
@@ -10,10 +10,13 @@ var URI = require("uri-js");
 var fs = require('fs');
 var jsonPath = require('JSONPath');
 var util = require('util');
+var encoder = require('encoder.js');
+var extract = require('url-querystring');
 
 var baseURI = 'http://ws.detectlanguage.com/0.2/detect';
 var key = '4392d0dfdf3a7344a500761f34ccf419';
-//var encodedURI = URI.serialize(URI.parse(baseURI + "?q=muito praze&key=4392d0dfdf3a7344a500761f34ccf419"));
+//Sample uri call
+//http://ws.detectlanguage.com/0.2/detect?q=muito praze&key=4392d0dfdf3a7344a500761f34ccf419"));
 
 fs.readFile('testData.json', 'utf8', function (err, data) {
     if (err) throw err; // we'll not consider error handling for now
@@ -28,7 +31,9 @@ fs.readFile('testData.json', 'utf8', function (err, data) {
         request(encodedURI, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 var fbResponse = JSON.parse(body);
-                console.log("Got a response: ", fbResponse.data.detections);
+                var query = encoder.decodeURI(this.href);
+                var uriObject = extract(query);
+                console.log("Got a response: ", fbResponse.data, " Text : ", uriObject.qs.q);
             } else {
                 console.log("Got an error: ", error, ", status code: ", response.statusCode);
             }
